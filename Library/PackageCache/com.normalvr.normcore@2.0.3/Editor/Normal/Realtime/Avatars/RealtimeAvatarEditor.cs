99b091b4293dd02a8acdbb6091ccab69f61b7a23
shanetomlinson <tiedupinside@gmail.com>
2021-04-08 08:47:25 -0600

    no message

--- Library/PackageCache/com.normalvr.normcore@2.0.3/Editor/Normal/Realtime/Avatars/RealtimeAvatarEditor.cs
+++ Library/PackageCache/com.normalvr.normcore@2.0.3/Editor/Normal/Realtime/Avatars/RealtimeAvatarEditor.cs
@@ -50,7 +50,7 @@ namespace Normal.Realtime {
 
 
             //// Head
-            Transform head = CreateGameObjectForPropertyIfNeeded(headProperty, gameObject.transform, "Head", new Type[]{ typeof(Examples.VoiceScale) });
+            Transform head = CreateGameObjectForPropertyIfNeeded(headProperty, gameObject.transform, "Head", new Type[]{ typeof(RealtimeAvatarVoiceScale) });
 
             // RealtimeView
             RealtimeView headRealtimeView = AddRealtimeViewComponentIfNeeded(head.gameObject);
