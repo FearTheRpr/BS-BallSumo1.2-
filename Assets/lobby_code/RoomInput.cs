37b6906191552e7e421334788ec24344f34378e0
Skylerh999 <skyler.harder@lethbridgecollege.ca>
2021-04-16 11:58:01 -0600

    Saved Data and Lobby doneadded scene and room joining

---
+++ Assets/lobby_code/RoomInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Adds characters or clears the room name
+/// To use:
+///
+/// Put this code on a Steamvr activator such as a button
+/// Set RN to a RoomName in the scene, there should only be one in the scene!
+/// Connect one of the functions to the on pressed/clicked event, set the public field to the character you want if it's AddCharacterToString
+///
+/// When the character presses a button, it will either input the string you want, or clear the room name, depending on what function.
+/// 
+/// </summary>
+public class RoomInput : MonoBehaviour
+{
+    
+
+    public RoomName rn; //roomName.cs to change
+    public Text Room;
+    
+    public void AddCharacterToRoomName(string c) //Adds c to the room name
+    {
+
+        rn.roomName += c;
+        Room.text = rn.roomName;
+    }
+
+    public void ClearRoomName()
+    {
+        rn.roomName = String.Empty; //Clears the room name
+        Room.text = "Cleared";
+    }
+    
+    
+
+}
