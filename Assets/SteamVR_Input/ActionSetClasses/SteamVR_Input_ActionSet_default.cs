17b716dc5eba3db4a00032e359c52888b264e549
shanetomlinson <tiedupinside@gmail.com>
2021-04-17 19:43:14 -0600

    no message

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Grab
+        {
+            get
+            {
+                return SteamVR_Actions.default_Grab;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
