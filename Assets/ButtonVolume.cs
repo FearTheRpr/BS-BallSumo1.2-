13364749fbbae124ef6b73f1cad9680b1a832c5b
shanetomlinson <tiedupinside@gmail.com>
2021-04-17 21:46:53 -0600

    input quit button

---
+++ Assets/ButtonVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ButtonVolume : MonoBehaviour
+{
+    public AudioMixer GameVolume;
+
+    public void SetLevel()
+    {
+        GameVolume.SetFloat("Music", Mathf.Log10( + 1 ) * 20);
+    }
+}
---
+++ Assets/QuitApp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+
+    public void Quit()
+    {
+
+    Application.Quit();
+    }
+}
