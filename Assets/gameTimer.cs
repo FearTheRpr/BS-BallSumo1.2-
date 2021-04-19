3bacf4e0eaeac822f174f42017b5ec460ded8214
shanetomlinson <tiedupinside@gmail.com>
2021-04-18 22:22:22 -0600

    ring now lowers at specific times, gradual lower does not work so 5 second warning before ring lowers

--- Assets/gameTimer.cs
+++ Assets/gameTimer.cs
@@ -8,6 +8,9 @@ public class gameTimer : RealtimeComponent<TimerModel>
 {
     private Text _timeText;
     public Realtime _realtime;
+    public GameObject _Ring1;
+    public GameObject _Ring2;
+   
 
     // the starting time limit in milliseconds (eg. 90000 = 90 seconds)
     public double _timeLimit;
@@ -22,6 +25,8 @@ public class gameTimer : RealtimeComponent<TimerModel>
         // subscribe to the DidConnectToRoom event
         _realtime.didConnectToRoom += DidConnectToRoom;
     }
+
+
 
     protected override void OnRealtimeModelReplaced(TimerModel previousModel, TimerModel currentModel)
     {
@@ -41,34 +46,69 @@ public class gameTimer : RealtimeComponent<TimerModel>
     {
         if (model.timerCode == _timeLimit / 1000)
         {
-            model.timerCode = _realtime.room.time + 600;
+            //change if different end time is desired (600 = 10 min)
+            model.timerCode = _realtime.room.time + 312;
         }
-        Debug.Log("DidConnect");
+
+       
+
         StartCoroutine("CountDown");
     }
     private IEnumerator CountDown()
     {
-         while (timeLeft > 0)
-         {
+        while (timeLeft > 0)
+        {
 
-             timeLeft = CompareDates(UnixTimeStampToDateTime(model.timerCode), UnixTimeStampToDateTime(_realtime.room.time));
+            timeLeft = CompareDates(UnixTimeStampToDateTime(model.timerCode), UnixTimeStampToDateTime(_realtime.room.time));
             // converts the time left into minutes 
             _timeText.text = ((int)(timeLeft / 60)).ToString() + ":";
 
-             // converts the remaining time (after minutes) to seconds
-             int seconds = (int)(timeLeft % 60);
-             // if seconds are less than 10, add a 0 at the beginning
-             if (seconds < 10)
-             {
-                 _timeText.text += "0" + ((int)(timeLeft % 60)).ToString();
-             }
-             else
-             {
-                 _timeText.text += ((int)(timeLeft % 60)).ToString();
-             }
-             yield return null;
-         }
-        
+            // converts the remaining time (after minutes) to seconds
+            int seconds = (int)(timeLeft % 60);
+            // if seconds are less than 10, add a 0 at the beginning
+            if (seconds < 10)
+            {
+                _timeText.text += "0" + ((int)(timeLeft % 60)).ToString();
+            }
+            else
+            {
+                _timeText.text += ((int)(timeLeft % 60)).ToString();
+            }
+            yield return null;
+
+
+
+            //lowers the outter ring when the timer says 5 min remaining
+            if (_timeText.text == "5:00" && _Ring1 != null)
+            {
+
+                Debug.Log("Time Left 5 min");
+                _Ring1.transform.position += new Vector3(0, -10, 0) * Time.deltaTime;
+                Destroy(_Ring1, 5);
+               
+            }
+            //lowers the outter ring when the timer says 2 asnd a half min remaining
+            if (timeLeft < 150 && _Ring2 != null)
+            {
+                Debug.Log("Time Left 2.5 min");
+                _Ring2.transform.position += new Vector3(0, -10, 0) * Time.deltaTime;
+                Destroy(_Ring2, 5);
+            }
+            //what happens when timer runs out
+            if (timeLeft == 0.01)
+            {
+                Debug.Log("Game Over");
+
+            }
+
+            else
+            {
+                        Debug.Log("waiting");
+            }
+
+
+        }
+
     }
 
     // used to get the difference between the current time and the stored startTime
@@ -87,4 +127,6 @@ public class gameTimer : RealtimeComponent<TimerModel>
         dtDateTime = dtDateTime.AddSeconds(unixTimeStamp);
         return dtDateTime;
     }
+
+
 }
