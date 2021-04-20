f0fe2b15f036fe92b420f998d9cd82a2405fabc1
shanetomlinson <tiedupinside@gmail.com>
2021-04-20 10:14:37 -0600

    Added reset code to personal UI.  Made prefabs out of all my items

---
+++ Assets/Scripts/ResetGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+{
+    public GameObject scene;
+
+    public void resetGame()
+    {
+        //resets the scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
